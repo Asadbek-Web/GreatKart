e3fca7b240e0e670bbc78c8ebbe8f3f4002334f6
Asdbek-Web <asadbekabdumalikovfifth@gmail.com>
2023-01-24 19:28:04 +0500

    store and carts functionality

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Product
-
+from .models import Product, Variation
 # Register your models here.
 
 
@@ -9,5 +8,9 @@ class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('product_name',)}
 
 
-admin.site.register(Product, ProductAdmin)
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value', 'created_date', 'is_active')
+    list_editable = ('is_active',)
 
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Variation, VariationAdmin)
--- store/views.py
+++ store/views.py
@@ -3,6 +3,10 @@ from .models import Product
 from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
+from django.http import HttpResponse
+from django.db.models import Q
+
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
 
@@ -10,24 +14,28 @@ def store(request, category_slug=None):
     categories = None
     products = None
 
-
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_prodcuts = paginator.get_page(page)
         product_count = products.count()
 
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_prodcuts = paginator.get_page(page)
         product_count = products.count()
 
-
     context = {
-        'products': products,
+        'products': paged_prodcuts,
         'product_count': product_count,
     }
-
-
     return render(request, 'store/store.html', context)
+
+
 
 
 def product_detail(request, category_slug, product_slug):
@@ -41,9 +49,25 @@ def product_detail(request, category_slug, product_slug):
         'single_product': single_product,
         'in_cart': in_cart,
     }
-
     return render(request, 'store/product_detail.html', context)
 
+
+
+
+def search(request):
+    products = ''
+    if 'key' in request.GET:
+        key = request.GET['key']
+        if key:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains = key) | Q(product_name__icontains = key))
+            product_count = products.count()
+
+
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context) 
 
 
 
@@ -51,3 +75,4 @@ def product_detail(request, category_slug, product_slug):
 
 
 # https://www.youtube.com/watch?v=N0QLc6H0n9Y django interview
+
