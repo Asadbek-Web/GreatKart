0b317093aa137d411f4519be9f28d9b8b38eacd1
Asdbek-Web <asadbekabdumalikovfifth@gmail.com>
2023-02-11 00:45:29 +0500

    database

--- greatcard/settings.py
+++ greatcard/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'n^#(g5t_s+awm!mm8lld49=hdad$0pn)a^ona37)h_l4-hv--^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool) #True
