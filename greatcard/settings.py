76a6f1657b157e5312cb77273f500accc207f445
Asdbek-Web <asadbekabdumalikovfifth@gmail.com>
2023-02-10 23:26:49 +0500

    add gunicorn

--- greatcard/settings.py
+++ greatcard/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool) #True
 
-ALLOWED_HOSTS = ['greatcart-course-env.eba-3xmnk2wp.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['127.0.0.1', ]
 
 
 # Application definition
